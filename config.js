5740e17eecb012a4957aa3399379a0c07d7f0257
Carla <carlakroll7@gmail.com>
2017-05-04 18:52:53 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  consumer_key: 'W0ZMU50THDYawv9duWy7ob8JI',
-  consumer_secret: 'KHfITzwXjLKyTXnWGd4QaivwB1Scceng6hLRXYuLBfLPgrE9kn',
-  access_token: '840235888328216578gkAF2EC7gIe4k2p1c4SCqxmHXfeLOMZ',
-  access_token_secret: 'Caue8vDNCe4vNSx8iL26khSwz20ZUxVzybl1B8bdtGKK7'
+  consumer_key: '',
+  consumer_secret: '',
+  access_token: '',
+  access_token_secret: ''
 }
